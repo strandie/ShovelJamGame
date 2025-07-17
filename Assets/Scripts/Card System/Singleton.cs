f9f3e5d9665d847b3d766659452ad2b7368c004b
ayinfly <ayinflyschool@gmail.com>
2025-07-16 22:22:27 -0700

    commented out card system template to prevent errors

--- Assets/Scripts/Card System/Singleton.cs
+++ Assets/Scripts/Card System/Singleton.cs
@@ -1,34 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
-{
-    public static T Instance { get; private set; }
+// public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+// {
+//     public static T Instance { get; private set; }
 
-    protected virtual void Awake()
-    {
-        if(Instance != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this as T;
-    }
+//     protected virtual void Awake()
+//     {
+//         if(Instance != null)
+//         {
+//             Destroy(gameObject);
+//             return;
+//         }
+//         Instance = this as T;
+//     }
 
-    protected virtual void OnApplicationQuit()
-    {
-        Instance = null;
-        Destroy(gameObject);
-    }
-}
+//     protected virtual void OnApplicationQuit()
+//     {
+//         Instance = null;
+//         Destroy(gameObject);
+//     }
+// }
 
-// Singleton persists when changing scenes
-public abstract class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour
-{
-    protected void Awake()
-    {
-        base.Awake();
-        DontDestroyOnLoad(gameObject);
-    }
-}
+// // Singleton persists when changing scenes
+// public abstract class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour
+// {
+//     protected void Awake()
+//     {
+//         base.Awake();
+//         DontDestroyOnLoad(gameObject);
+//     }
+// }
